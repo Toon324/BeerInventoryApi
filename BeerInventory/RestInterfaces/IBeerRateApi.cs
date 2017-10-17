f269d06916c58b08555579c88d2c79c18befdd51
Cody Swendrowski <cody.swendrowski@7summitsinc.com>
2017-10-17 17:59:35 -0500

    Adds BeerRate API

---
+++ BeerInventory/RestInterfaces/IBeerRateApi.cs
@@ -0,0 +1,20 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BeerInventory.RestInterfaces
+{
+    public interface IBeerRateApi
+    {
+        [Get("/upc.asp?k=2e1ob20b6n0gc999r")]
+        Task<dynamic> GetByUpc(string upc);
+    }
+
+    public class BeerRateApi
+    {
+        public const string ApiUrl = "https://www.ratebeer.com/json";
+    }
+}
